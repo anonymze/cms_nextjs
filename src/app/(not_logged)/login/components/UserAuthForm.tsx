5ad2db009a9ca100c73b66af238fd6c4ffb0fe38
Yann <anodevfr@gmail.com>
2023-12-27 00:33:32 +0100

    feat: logic login complete

--- src/app/(not_logged)/login/components/UserAuthForm.tsx
+++ src/app/(not_logged)/login/components/UserAuthForm.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Input } from "@/components/Form/Input";
 import { Label } from "@/components/Form/Label";
 import { Button } from "@/components/ui/Button";
@@ -7,41 +6,104 @@ import { SpinnerLoader } from "@/components/ui/Loader/Loader";
 import { cn } from "@/utils/libs/shadcn";
 import { useSignUp } from "@clerk/nextjs";
 import { GithubIcon } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
-
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
   const { isLoaded: isClerkLoaded, signUp, setActive } = useSignUp();
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const onSubmit = async (event: React.SyntheticEvent) => {
-    event.preventDefault();
+  const onSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
     setIsLoading(true);
 
     if (!isClerkLoaded) {
-      console.error('Clerk is not loaded yet');
+      console.error("Clerk is not loaded yet");
       return;
     }
 
     await signUp
       .create({
-        emailAddress: "t@gmail.com",
-        password: "oki",
+        emailAddress: ev.currentTarget.email.value,
+        password: ev.currentTarget.email.value,
       })
       .then(async (result) => {
-        // Send the user an email with the verification code
-        await signUp.prepareEmailAddressVerification({
+        // send the user an email with the verification code
+        await result.prepareEmailAddressVerification({
           strategy: "email_code",
         });
 
-        // Set 'verifying' true to display second form and capture the OTP code
-        // setVerifying(true);
+        const params = new URLSearchParams(searchParams);
+        params.set("verifying", result.emailAddress || "");
+        router.push((pathname + "?" + params.toString()) as __next_route_internal_types__.RouteImpl<string>);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  const onSubmitCode = async (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    setIsLoading(true);
+
+    if (!isClerkLoaded) {
+      console.error("Clerk is not loaded yet");
+      return;
+    }
+
+    // verify the code in email
+    signUp
+      .attemptVerification({
+        strategy: "email_code",
+        code: ev.currentTarget.code.value,
+      })
+      .then((result) => {
+        setIsLoading(false);
+
+        if (result.status === "complete") {
+          setActive({ session: result.createdSessionId });
+          router.push(
+            (searchParams.get("redirect_url") ||
+              "/dashboard") as __next_route_internal_types__.RouteImpl<string>,
+          );
+        }
       })
       .catch((err) => {
         setIsLoading(false);
       });
   };
+
+  if (searchParams.get("verifying")) {
+    return (
+      <form onSubmit={onSubmitCode} autoComplete="off">
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="code">
+              Code de vérification
+            </Label>
+            <Input
+              name="code"
+              id="code"
+              placeholder="123456"
+              type="text"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && <SpinnerLoader />}
+            Connexion avec email
+          </Button>
+        </div>
+      </form>
+    );
+  }
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
@@ -52,6 +114,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               Email
             </Label>
             <Input
+              name="email"
               id="email"
               placeholder="nom@exemple.fr"
               type="email"
@@ -65,6 +128,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               Mot de passe
             </Label>
             <Input
+              name="password"
               id="password"
               placeholder="mot de passe"
               type="password"
--- src/app/(not_logged)/login/page.tsx
+++ src/app/(not_logged)/login/page.tsx
@@ -2,7 +2,8 @@ import { cn } from "@/utils/libs/shadcn";
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import { UserAuthForm } from "./components/user-auth-form";
+import { UserAuthForm } from "./components/UserAuthForm";
+import { Activity } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -16,18 +17,7 @@ export default function AuthenticationPage() {
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
+            <Activity className="mr-1" />
             Powered by Clerk
           </div>
           <div className="relative z-20 mt-auto">
