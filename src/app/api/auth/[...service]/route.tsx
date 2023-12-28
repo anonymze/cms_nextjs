6bc36f4786895c3512d97503fd171a126c7a2bb1
Yann <anodevfr@gmail.com>
2023-12-29 00:32:58 +0100

    refacto: route auth

--- src/app/api/auth/[...service]/route.tsx
+++ src/app/api/auth/[...service]/route.tsx
@@ -1,27 +1,33 @@
 import { api } from "@/api/_config";
 import { ENV_SERVER } from "@/env/server";
 import prisma from "@/utils/libs/prisma";
-import { Clerk } from "@clerk/clerk-sdk-node";
-import type { NextRequest } from "next/server";
+import { clerkClient } from "@clerk/nextjs";
+import { NextResponse, type NextRequest } from "next/server";
 
-const OAUTH_SERVICES = ["github", "google", "apple"] as const;
+const OAUTH_SERVICES = ["github", "google", "apple"];
 
 export async function GET(req: NextRequest) {
+  /***/
   const service = req.nextUrl.pathname.split("/").at(-1);
-  const searchParams = req.nextUrl.searchParams;
 
-  // @ts-ignore, i don't know how to do...
   if (!service || !OAUTH_SERVICES.includes(service)) {
     return new Response("Service OAUTH not handled");
   }
 
+  try {
+    verifyEnvVariables(service);
+  } catch (err) {
+    if (err instanceof Error) return new Response(err.message);
+  }
+
+  /***/
+  const searchParams = req.nextUrl.searchParams;
+
   const code = searchParams.get("code");
   const state = searchParams.get("state");
-
-  const error = searchParams.get("error");
   const errorDescription = searchParams.get("error_description");
 
-  if (error) {
+  if (errorDescription) {
     return new Response(errorDescription);
   }
 
@@ -29,17 +35,10 @@ export async function GET(req: NextRequest) {
     return new Response("Oauth invalid data");
   }
 
-  if (!ENV_SERVER.NEXT_PUBLIC_GITHUB_CLIENT_ID || !ENV_SERVER.NEXT_PUBLIC_GITHUB_CLIENT_ID) {
-    return new Response("Client ID or secret is not set in your env");
-  }
-
-  if (!ENV_SERVER.GITHUB_ACCESS_TOKEN_URL) {
-    return new Response("Github URL access token not set");
-  }
-
+  /***/
   try {
     const accessToken = await api.post(
-      ENV_SERVER.GITHUB_ACCESS_TOKEN_URL,
+      ENV_SERVER.GITHUB_ACCESS_TOKEN_URL as string,
       {
         client_id: ENV_SERVER.NEXT_PUBLIC_GITHUB_CLIENT_ID,
         client_secret: ENV_SERVER.GITHUB_CLIENT_SECRET,
@@ -52,43 +51,90 @@ export async function GET(req: NextRequest) {
     );
 
     if (accessToken.data?.error) {
-      return new Response(accessToken.data.error_description);
+      return new Response(accessToken.data?.error_description);
     }
 
-    if (!ENV_SERVER.GITHUB_USER_URL) {
-      return new Response("URL user github not set");
+    const userGithub = await api.get(ENV_SERVER.GITHUB_USER_URL as string, {
+      headers: { Authorization: `Bearer ${accessToken.data.access_token}` },
+    });
+
+    const userGithubEmail = userGithub.data.email;
+
+    const existingUserOurDb = await prisma.user.findUnique({
+      where: { email: userGithubEmail },
+    });
+
+    // if user is created in our database and is active, we can return the redirection to the dashboard safely
+    if (existingUserOurDb?.isActive) {
+      return NextResponse.redirect(`${req.nextUrl.origin}/dashboard`);
     }
 
-    try {
-      const user = await api.get(ENV_SERVER.GITHUB_USER_URL, {
-        headers: { Authorization: `Bearer ${accessToken.data.access_token}` },
+    // otherwise we create the user in Clerk, but before, if already exists we get it from Clerk
+    const usersClerk = await clerkClient.users.getUserList({
+      emailAddress: [userGithubEmail],
+    });
+
+    const userClerk =
+      usersClerk.length > 0
+        ? usersClerk[0]
+        : await clerkClient.users.createUser({
+            emailAddress: [userGithubEmail],
+            firstName: userGithub.data.name || "NO_NAME",
+            // generate random password (we don't care, it just needs to be strong)
+            password: Math.random().toString(36) + Math.random().toString(36).slice(2),
+          });
+
+    // if user does not exists in my database at all, we create it
+    if (!existingUserOurDb) {
+      await prisma.user.create({
+        data: {
+          email: userGithubEmail,
+          name: userGithub.data.name,
+          isActive: false,
+        },
       });
-
-      const userEmail = user.data.email;
-
-      const existingUser = await prisma.user.findUnique({
-        where: { email: userEmail },
-      });
-
-      if (existingUser?.isActive) {
-        return new Response("oui");
-      }
-
-      if (!existingUser) {
-        await prisma.user.create({
-          data: {
-            email: userEmail,
-            name: user.data.name,
-            isActive: false,
-          },
-        });
-      }      
-
-      return new Response("oui mais pas actif");
-    } catch (error: any) {
-      return new Response(error.response.statusText, { status: error.response.status });
     }
-  } catch (error: any) {
-    return new Response(error.response.statusText, { status: error.response.status });
+
+    return NextResponse.redirect(`${req.nextUrl.origin}/dashboard`);
+  } catch (error) {
+    if (error instanceof Response) {
+      return new Response(error.statusText, { status: error.status });
+    }
+
+    if (error instanceof Error) {
+      return new Response(error.message);
+    }
+
+    return new Response("Erreur inconnue, contactez l'administrateur");
+  }
+}
+
+function verifyEnvVariables(service: string): void {
+  switch (service) {
+    case "github":
+      verifyGithubEnvVariables();
+      break;
+    case "google":
+      break;
+    case "apple":
+      break;
+  }
+}
+
+function verifyGithubEnvVariables(): void {
+  if (!ENV_SERVER.NEXT_PUBLIC_GITHUB_CLIENT_ID) {
+    throw new Error("Client ID is not set in your env");
+  }
+
+  if (!ENV_SERVER.NEXT_PUBLIC_GITHUB_CLIENT_ID) {
+    throw new Error("Client secret is not set in your env");
+  }
+
+  if (!ENV_SERVER.GITHUB_ACCESS_TOKEN_URL) {
+    throw new Error("Github URL access token is not set in your env");
+  }
+
+  if (!ENV_SERVER.GITHUB_USER_URL) {
+    throw new Error("URL user github is not set in your env");
   }
 }
