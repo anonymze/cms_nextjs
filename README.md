f3c6e148fb2c5ca64d3200e6e0cb214e6fba30fe
anonymze <anodevfr@gmail.com>
2024-01-25 22:37:16 +0100

    fix: api routes

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ _npm run knip to detect unused dependencies, files, and more..._
 
 <!-- TODO -->
 
-* fix api routes with clerk
+* 401 route
 * in content show page and article (not only article)
 * add images in page/article form
 * add an update page per image, with description, tag etc... with i18n handled
+* 404 routes
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,12 @@
 import { NextResponse } from "next/server";
 import { ENV_SERVER } from "./env/server";
-import type { NextRequest } from "next/server";
 import { authMiddleware } from "@clerk/nextjs";
-
-type TokenInfo =
-  | {
-      validToken: true;
-      token: string;
-    }
-  | {
-      validToken: false;
-      token: null;
-    };
+import type { NextRequest } from "next/server";
 
 export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
@@ -25,43 +14,41 @@ export const config = {
      * - register (authentication routes)
      * - conditions utilisation cgu (legal routes)
      * - politique-confidentialite (legal routes)
+     * - test (in case you wanna make a test route)
+     * - 401 (unauthorized route)
      */
-    "/((?!_next/static|_next/image|favicon.ico|test|login.*|register.*|politique-de-confidentialite|cgu|api/.*).*)",
+    "/((?!_next/static|_next/image|favicon.ico|test|login.*|register.*|politique-de-confidentialite|cgu|401).*)",
   ],
 };
 
 export default authMiddleware({
   beforeAuth: (req) => {
-    // / redirect to /dashboard
+    // "/" redirect to /dashboard
     if (req.nextUrl.pathname === "/") {
       return NextResponse.redirect(new URL("dashboard", req.nextUrl.origin));
     }
 
     if (req.nextUrl.pathname.startsWith("/api")) {
-      const { validToken, token } = tokenInfo(req);
+      // if we got a valid token api in the request, we skip the clerk authentication with next()
+      if (hasValidToken(req)) return NextResponse.next();
 
-      // token no valid = 401 unauthorized from clerk, you can redirect here if you prefer
-      if (!validToken) return;
-
-      // console.log(token);
-
-      return false;
+      return NextResponse.redirect(new URL("/401", req.url));
     }
 
     return;
   },
 });
 
-const tokenInfo = (request: NextRequest): TokenInfo => {
+const hasValidToken = (request: NextRequest) => {
   const tokenCookie = request.cookies.get("token")?.value;
 
   // 2 authentications authorized, if we get the token by cookies
-  if (tokenCookie === ENV_SERVER.API_KEY) return { validToken: true, token: tokenCookie };
+  if (tokenCookie === ENV_SERVER.API_KEY) return true;
 
   const tokenAuth = request.headers.get("authorization");
 
   // or if we get the token by authorization header
-  if (tokenAuth === ENV_SERVER.API_KEY) return { validToken: true, token: tokenAuth };
+  if (tokenAuth === ENV_SERVER.API_KEY) return true;
 
-  return { validToken: false, token: null };
+  return false;
 };
